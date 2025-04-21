b4c2ae2d08e86537ea886f4be9b815843f762289
sanket Yadav <yadavsanket663@gmail.com>
2025-04-21 09:30:24 +0530

    Working on frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Navigation from './customer/components/Navigation/Navigation'
+import Homepage from './customer/Pages/Homepage/Homepage'
 function App() {
+
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+      <Navigation/>
+    </div>
+    <div>
+      <Homepage/>
+    </div>
     </>
   )
 }
---
+++ src/customer/Pages/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCarosel from '../../components/HomeCarosel/MainCarosel'
+
+function Homepage() {
+  return (
+    <div>
+      <MainCarosel/>
+      <div>
+        Other Section
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
